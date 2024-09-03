3cfbe932fbf7259110316dc93c48dc3a0f1cd90b
Eeshaan Sarda <sardaeeshaan@gmail.com>
2024-09-03 20:51:04 +0545

    created Homescreen

--- QuizGameRN/app/index.tsx
+++ QuizGameRN/app/index.tsx
@@ -1,15 +1,17 @@
-import { Text, View } from "react-native";
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { Link } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-export default function Index() {
+export default function HomeScreen() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView className="flex-1 items-center bg-white">
+      <Text className="text-4xl font-bold mb-8">Quiz Game</Text>
+      <Link href="/quiz" asChild>
+        <TouchableOpacity className="bg-blue-500 px-10 py-4 rounded-lg">
+          <Text className="text-xl text-white font-semibold">Start Quiz</Text>
+        </TouchableOpacity>
+      </Link>
+    </SafeAreaView>
   );
 }
