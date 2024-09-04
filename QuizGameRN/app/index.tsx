b44617534be866ffb9ac7c69fff2317d062a610a
Eeshaan Sarda <sardaeeshaan@gmail.com>
2024-09-04 16:36:28 +0545

    added images and styled

--- QuizGameRN/app/index.tsx
+++ QuizGameRN/app/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
 import { Link } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function HomeScreen() {
   return (
     <SafeAreaView className="flex-1 bg-white items-center justify-center px-6 py-10">
+      <Image 
+        source={require('../assets/images/quiz.png')}
+        className="w-48 h-48 mb-10"
+        resizeMode='contain'
+      />
       <Text className="text-5xl font-extrabold text-blue-700 mb-20 tracking-wide">
         Quiz Game
       </Text>
--- QuizGameRN/app/result.tsx
+++ QuizGameRN/app/result.tsx
@@ -1,37 +1,7 @@
-/*
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useLocalSearchParams, router } from 'expo-router';
-
-export default function ResultScreen() {
-  const { score } = useLocalSearchParams<{ score: string }>();
-
-  const handleRestart = () => {
-    router.replace('/');
-  };
-
-  return (
-    <SafeAreaView className="flex-1 bg-white">
-      <View className="flex-1 justify-center items-center p-4">
-        <Text className="text-2xl font-bold mb-16">Quiz Completed!</Text>
-        <Text className="text-xl mb-16">Your Score: {score}</Text>
-        <TouchableOpacity
-          onPress={handleRestart}
-          className="bg-blue-500 w-full p-4 rounded-lg"
-        >
-          <Text className="text-white text-center font-semibold">Restart Quiz</Text>
-        </TouchableOpacity>
-      </View>
-    </SafeAreaView>
-  );
-}
-*/
-import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { useLocalSearchParams, router } from 'expo-router';
-import { LinearGradient } from 'expo-linear-gradient';
 
 export default function ResultScreen() {
   const { score } = useLocalSearchParams<{ score: string }>();
@@ -41,14 +11,19 @@ export default function ResultScreen() {
 
   return (
     <SafeAreaView className="bg-white flex-1 justify-center items-center p-6">
-      <Text className="text-4xl font-bold mb-16">Quiz Completed!</Text>
-      <View className="bg-blue-100 rounded-full w-40 h-40 justify-center items-center mb-16">
+      <Image 
+        source={require('../assets/images/score.png')}
+        className="w-48 h-48"
+        resizeMode='contain'
+      />
+      <Text className="text-4xl text-blue-700 font-bold mb-8">Quiz Completed!</Text>
+      <View className="bg-blue-100 rounded-xl w-40 h-40 justify-center items-center mb-16">
         <Text className="text-5xl font-bold">{score}</Text>
         <Text className="text-xl">Score</Text>
       </View>
       <TouchableOpacity
         onPress={handleRestart}
-        className="bg-blue-500 w-full p-4 rounded-lg shadow-md active:opacity-80"
+        className="bg-blue-500 w-3/4 p-4 rounded-lg shadow-md active:opacity-80"
       >
         <Text className="text-white text-center font-bold text-lg">Restart Quiz</Text>
       </TouchableOpacity>
